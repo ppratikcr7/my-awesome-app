14a4e4460a8bcd1e2240ee3a6a38ae92710b89df
ppratikcr7 <ppratik.cr7@gmail.com>
2021-06-17 02:32:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const APP_PORT = process.env.APP_PORT || 3000;app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+app.listen(APP_PORT, () => {
+  console.log(`Running app at port:${APP_PORT}`)
+})
